d386f2c2b5e83bfd7abe2350e304b91ba405a9fd
Krzysztof Druciarek <kkd26@cam.ac.uk>
2021-10-20 18:05:16 +0100

    Data Abstraction

---
+++ dataAbstraction.ml
@@ -0,0 +1,39 @@
+module type BOOL = sig
+  type t
+
+  val yes : t
+
+  val no : t
+
+  val choose : t -> 'a -> 'a -> 'a
+end
+
+module M1 : BOOL = struct
+  type t = int
+
+  let yes = 1
+
+  let no = 0
+
+  let choose b e1 e2 = if b = 1 then e1 else e2
+end
+
+module M2 : BOOL = struct
+  type t = unit option
+
+  let yes = Some ()
+
+  let no = None
+
+  let choose b e1 e2 = match b with Some () -> e1 | None -> e2
+end
+
+module M3 : BOOL = struct
+  type t = { f : 'a. 'a -> 'a -> 'a }
+
+  let yes = { f = (fun a b -> a) }
+
+  let no = { f = (fun a b -> b) }
+
+  let choose b e1 e2 = b.f e1 e2
+end
